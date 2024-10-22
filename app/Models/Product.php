dc29933eb4e593e21ebf6781b7ef0b9e1ec606c8
Amr Elsayed <amr-elsayed@outlook.com>
2024-10-23 00:32:14 +0300

    add migrations and models

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'price',
+        'stock',
+        'category_id'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class);
+    }
+}
