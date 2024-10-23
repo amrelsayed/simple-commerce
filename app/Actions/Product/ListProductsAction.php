b1d9b809140c5d030fa4d9db8ba381f558522cec
Amr Elsayed <amr-elsayed@outlook.com>
2024-10-23 13:27:31 +0300

    refactor to actions

---
+++ app/Actions/Product/ListProductsAction.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Actions\Product;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class ListProductsAction
+{
+    public function execute(Request $request): LengthAwarePaginator
+    {
+        // initiate query
+        $query = Product::query();
+
+        // filter query
+        if ($request->has("name")) {
+            $query->where("name", "like", "%" . $request->name . "%");
+        }
+
+        if ($request->has("price_from")) {
+            $query->where("price", '>=', $request->price_from);
+        }
+
+        if ($request->has("price_to")) {
+            $query->where("price", '<=', $request->price_to);
+        }
+
+        if ($request->has("category_id")) {
+            $query->whereHas("category", function ($query) use ($request) {
+                $query->where("id", $request->category_id);
+            });
+        }
+
+        // load category
+        $query->with("category");
+
+        return $query->paginate();
+    }
+}
