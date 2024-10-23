a419b50575c77d8ca1d6998e8f28deb539d72c8c
Amr Elsayed <amr-elsayed@outlook.com>
2024-10-23 14:50:49 +0300

    add caching to products

--- app/Actions/Product/ListProductsAction.php
+++ app/Actions/Product/ListProductsAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Product;
 use App\Models\Product;
+use Cache;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 
@@ -9,31 +10,35 @@ class ListProductsAction
 {
     public function execute(Request $request): LengthAwarePaginator
     {
-        // initiate query
-        $query = Product::query();
+        $products = Cache::remember("products", 60 * 60, function () use ($request) {
+            // initiate query
+            $query = Product::query();
 
-        // filter query
-        if ($request->has("name")) {
-            $query->where("name", "like", "%" . $request->name . "%");
-        }
+            // filter query
+            if ($request->has("name")) {
+                $query->where("name", "like", "%" . $request->name . "%");
+            }
 
-        if ($request->has("price_from")) {
-            $query->where("price", '>=', $request->price_from);
-        }
+            if ($request->has("price_from")) {
+                $query->where("price", '>=', $request->price_from);
+            }
 
-        if ($request->has("price_to")) {
-            $query->where("price", '<=', $request->price_to);
-        }
+            if ($request->has("price_to")) {
+                $query->where("price", '<=', $request->price_to);
+            }
 
-        if ($request->has("category_id")) {
-            $query->whereHas("category", function ($query) use ($request) {
-                $query->where("id", $request->category_id);
-            });
-        }
+            if ($request->has("category_id")) {
+                $query->whereHas("category", function ($query) use ($request) {
+                    $query->where("id", $request->category_id);
+                });
+            }
 
-        // load category
-        $query->with("category");
+            // load category
+            $query->with("category");
 
-        return $query->paginate();
+            return $query->paginate();
+        });
+
+        return $products;
     }
 }
